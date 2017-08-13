fa9defd3ce56de0d74fe559d5e5b09d0b15fdea2
osirissc2 <burstein.alan@gmail.com>
2017-08-13 11:30:10 -0400

    port fix

--- sample.js
+++ sample.js
@@ -87,7 +87,8 @@ function runGoogle(auth,callback){
   });
 }
 
-app.listen(3000, function () {
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
   console.log('** Listening on port 3000!');
 })
 
