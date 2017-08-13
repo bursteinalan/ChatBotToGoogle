92bc46f97ab1ba04aa8f5d8965e4ffc6a37776a2
osirissc2 <burstein.alan@gmail.com>
2017-08-13 12:11:19 -0400

    debugging stuff

--- sample.js
+++ sample.js
@@ -20,6 +20,7 @@ app.get('/', function (req, res) {
 })
 
 app.post('/sheets', function(req, res){
+  console.log("Received: ",req)
   console.log(req.body)
     var name = req.body.name;
     console.log("** Received request for: ",name);
